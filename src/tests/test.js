c4f2241d4f07877084153b66dad4e6ee21a02300
naandan <nandanramdani608@gmail.com>
2024-03-16 16:26:23 +0700

    update

--- src/tests/test.js
+++ src/tests/test.js
@@ -1,19 +1,12 @@
 import request from "supertest";
-import { expect } from "chai"; // Tambahkan pengimporan ini
 import app from "../app.js";
 
 describe("Server", () => {
   it('should return status 200 and display "Hello, World!"', async () => {
-    try {
-      const res = await request(app).get("/");
-      console.log(res.status);
-      console.log(res.headers["content-type"]);
-      console.log(res.text);
-      expect(res.status).to.equal(200);
-      expect(res.headers["content-type"]).to.contain("text/html");
-      expect(res.text).to.contain("Hello World!");
-    } catch (err) {
-      throw err;
-    }
+    request(app)
+      .get("/")
+      .expect(200)
+      .expect("Content-Type", /text\/html/)
+      .expect("Hello World!");
   });
 });
